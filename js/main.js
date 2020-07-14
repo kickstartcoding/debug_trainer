54e2bb9ff50034d34f623d767c176e9d9cdaa04f
neurodynamic <>
2020-07-14 16:35:56 -0700

    removes nexe; switches to using js file with #!env comment

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function run() {
       dataFilePath: SavedData.dataFilePath,
       workingDirectory: process.cwd(),
       data: data,
-      versionMessage: "2.0.0"
+      versionMessage: "3.0.0"
     }
   })
 
