633483df1a54c0f8aa434faceef2f1f982b51cfa
neurodynamic <>
2020-07-26 09:38:17 -0700

    build and version bump

--- js/main.ts
+++ js/main.ts
@@ -21,7 +21,7 @@ export function run(): void {
       dataFilePath: SavedData.dataFilePath,
       workingDirectory: process.cwd(),
       data: data,
-      versionMessage: "6.0.1"
+      versionMessage: "6.2.0"
     }
   })
 
